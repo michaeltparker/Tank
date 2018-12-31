f3142ac88963954841d317f6d1e5f285b53a7394
Michael Parker <michaeltparker@gmail.com>
2018-12-31 14:31:53 -0800

    Update tankreader.ino
    Resorted imported code and updated interface pins as there was a conflict between hardware

--- tankreader.ino
+++ tankreader.ino
@@ -33,6 +33,38 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE
+ *  
+ * 
+ * Description:
+ *  Connect the ultrasonic sensor to the Arduino as per the
+ *  hardware connections below. Run the sketch and open a serial
+ *  monitor. The distance read from the sensor will be displayed
+ *  in centimeters and inches.
+ *
+ * Hardware Connections used in this software:
+ *  Arduino | HC-SR04 (Ultrasonic Sensor)
+ *  -------------------
+ *    5V    |   VCC     
+ *    12    |   Trig     
+ *    13    |   Echo     
+ *    GND   |   GND
+ * 
+ *  Arduino | SH5461AS (Quad 7-Segment Display)
+ *  -------------------    
+ *    6     |   com1     
+ *    9     |   com2 
+ *    10    |   com3
+ *    0     |   com4
+ *    7     |   Seg a
+ *    11    |   Seg b
+ *    2     |   Seg c
+ *    4     |   Seg d
+ *    5     |   Seg e  
+ *    8     |   Seg f
+ *    1     |   Seg g
+ *    3     |   DP (decimal point)
+ *    
+ * 
  */
 
 #include <Arduino.h>
@@ -42,61 +74,34 @@
 #include "Digit.h"
 #include "Display.h"
 
+// Pins for SH5461AS
 const byte digit_pins[4] {6, 9, 10, 0}; // From com1 - com4
 const byte segment_pins[7] {7, 11, 2, 4, 5, 8, 1}; // From a - g (abc...g)
 const byte dp_pin = 3;
 
-Display d(digit_pins, segment_pins, dp_pin);
-
-void setup() {
-  for (int i = 0; i <= A5; i++) {
-    pinMode(i, OUTPUT);
-  }
-}
-
-void loop() {
-  d.print("1.234"); // Must be looped!
-}
-
-//IMPORT from HC-SR04 code to be resorted
-/**
- * HC-SR04 Demo
- * Demonstration of the HC-SR04 Ultrasonic Sensor
- * Date: August 3, 2016
- * 
- * Description:
- *  Connect the ultrasonic sensor to the Arduino as per the
- *  hardware connections below. Run the sketch and open a serial
- *  monitor. The distance read from the sensor will be displayed
- *  in centimeters and inches.
- * 
- * Hardware Connections:
- *  Arduino | HC-SR04 
- *  -------------------
- *    5V    |   VCC     
- *    7     |   Trig     
- *    8     |   Echo     
- *    GND   |   GND
- *  
- * License:
- *  Public Domain
- */
-
-// Pins
-const int TRIG_PIN = 7;
-const int ECHO_PIN = 8;
+// Pins for HC-SR04
+const int TRIG_PIN = 12; //updated from 7 in copied software
+const int ECHO_PIN = 13; //Updated from 8 in copied software
 
 // Anything over 400 cm (23200 us pulse) is "out of range"
 const unsigned int MAX_DIST = 23200;
 
-void setup() {
+//Initiate Display class object
+Display d(digit_pins, segment_pins, dp_pin);
 
+void setup() {
+  //Need to revist the use of this setup script
+  for (int i = 0; i <= A5; i++) {
+    pinMode(i, OUTPUT);
+  }
+   
   // The Trigger pin will tell the sensor to range find
   pinMode(TRIG_PIN, OUTPUT);
   digitalWrite(TRIG_PIN, LOW);
 
   // We'll use the serial monitor to view the sensor output
   Serial.begin(9600);
+  
 }
 
 void loop() {
@@ -138,6 +143,11 @@ void loop() {
     Serial.println(" in");
   }
   
+   //Call the print functionn from the display object to illuminate the value
+  d.print("1.234"); // Must be looped!
+  
   // Wait at least 60ms before next measurement
+  // NEED to revisit this approach, may need to consider a timer (or interrupt) to work with display
   delay(60);
+    
 }
